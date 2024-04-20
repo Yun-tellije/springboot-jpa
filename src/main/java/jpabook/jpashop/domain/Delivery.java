6d6c384c284cd2609f1258c420a362b354a4b62f
Yun-tellije <dbswjddms517@naver.com>
2024-04-20 19:56:12 +0900

    내용 수정

--- src/main/java/jpabook/jpashop/domain/Delivery.java
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -4,8 +4,6 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-import static jakarta.persistence.FetchType.LAZY;
-
 @Entity
 @Getter
 @Setter
@@ -15,7 +13,7 @@ public class Delivery {
     @Column(name = "delivery_id")
     private Long id;
 
-    @OneToOne(mappedBy = "delivery", fetch = LAZY)
+    @OneToOne(mappedBy = "delivery", fetch = FetchType.LAZY)
     private Order order;
 
     @Embedded
